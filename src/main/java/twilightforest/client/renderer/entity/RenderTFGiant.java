c12f4613590ccc87cea17808c9482e160fa00513
DrParadox7 <gustavo.caliari@live.com>
2023-03-11 19:49:33 +0100

    SkinPort Compat
    Adds Twilight Forest's Giant render compat with SkinPort

--- src/main/java/twilightforest/client/renderer/entity/RenderTFGiant.java
+++ src/main/java/twilightforest/client/renderer/entity/RenderTFGiant.java
@@ -1,20 +1,22 @@
 package twilightforest.client.renderer.entity;
 
-import net.minecraft.client.Minecraft;
+import cpw.mods.fml.common.Loader;
+import lain.mods.skinport.impl.forge.SkinPortModelPlayer;
 import net.minecraft.client.model.ModelBiped;
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
-
-import org.lwjgl.opengl.GL11;
 
 public class RenderTFGiant extends RenderBiped {
 
     private ResourceLocation textureLoc;
 
     public RenderTFGiant() {
-        super(new ModelBiped(), 0.625F);
+        super(skinCompat(), 0.625F);
 
         this.textureLoc = new ResourceLocation("textures/entity/steve.png");
     }
@@ -29,10 +31,16 @@ public class RenderTFGiant extends RenderBiped {
             return textureLoc;
         }
     }
+    /**
+     * Compat for SkinPort
+     */
+    private static ModelBiped skinCompat() {
+        return Loader.isModLoaded("skinport") ? new SkinPortModelPlayer(0.0F, false) : new ModelBiped();
+    }
 
     /**
-     * Allows the render to do any OpenGL state modifications necessary before the model is rendered. Args:
-     * entityLiving, partialTickTime
+     * Allows the render to do any OpenGL state modifications necessary before the model is rendered.
+     * Args: entityLiving, partialTickTime
      */
     protected void preRenderCallback(EntityLivingBase par1EntityLivingBase, float par2) {
         float scale = 4.0F;
